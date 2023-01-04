365105716bdfdad0e55eedcda768ab98a7adce69
Rashed Arman <rashedarman21@gmail.com>
2023-01-04 12:00:51 +0600

    Render fetched scores to DOM

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
+import { getScores } from './api';
 import './reset.css';
 import './styles.css';
+
+const refreshBtn = document.querySelector('.refresh-btn');
+const scoreBoard = document.querySelector('.score-board');
+
+const renderScores = async () => {
+  const { result: scores } = await getScores();
+  if (!scores) return null;
+
+  for (const { user, score } of scores) {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+      <td>${user}</td>
+      <td>${score}</td>
+    `;
+    scoreBoard.appendChild(tr);
+  }
+};
+
+refreshBtn.addEventListener('click', renderScores);
